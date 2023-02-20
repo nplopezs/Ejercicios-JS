06ece96ff8123db8beef264b9a62c57f146ac668
NatLop <nplopez_s@hotmail.com>
2023-02-20 16:37:17 -0600

    actualización

---
+++ Arrays.js
@@ -0,0 +1,17 @@
+// Promedio antipode
+// Crear una funcion que tome 2 parametros, (numero, longitud) y regrse un array de longitud cantidad de numeros multiplos del numero por parametro
+// arrayMultiplos(2, 10) -> [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// arrayMultiplos(17, 6) -> [17, 34, 51, 68, 85, 102]
+
+
+// Escrbir una funcion que descubra si el array es dominante positivo
+// Un array es positivo dominante cuando la mayoria de sus elementos son positivos -> [1, -2, 55, 10]
+// positiveDom([-1, -3, -5, 4, 6767]) -> false
+
+
+// Promedio antipode
+// Dado un array, devolver un array de menor longitud de acuerdo a los siguentes pasos:
+// - Dividir el array en 2 partes iguales*. Si no son iguales, eliminar el elemento en medio para obtener 2 arrays iguales
+// - Sumar cada numero de la primera parte con los numeros inversos de la segunda parte
+// [1,2,3] [5,22,6] -> 1 + 6, 2 + 22, 3 + 5 -> [7, 24, 8]
+// - Dividir cada numero del array final entre 2  -> [3.5, 12, 4]
--- ejerciciosJS.js
+++ ejerciciosJS.js
@@ -46,7 +46,7 @@ const laborHour = hour => {
     }
     return order;
 }
-console.log(new Date().getHours());
+//console.log(new Date().getHours());
 //console.log(laborHour(8));
 
 const verifyHorario = (day,hour) => {
@@ -56,7 +56,7 @@ const verifyHorario = (day,hour) => {
         console.log("Horario No Laboral");
     }
 }
-verifyHorario(new Date().getDay(),22);
+verifyHorario(new Date().getDay(),new Date().getHours());
 console.log("\n");
 
 // Escribir una funcion que regrese la cantidad de valores true que hay en un array
@@ -88,11 +88,42 @@ console.log("\n");
 // Encontrar los primeros n elementos de la serie fibonacci
 // Usar recursion
 console.log("Ejercicio 4");
-const fibo = n =>{
+
+/*const fibo = n =>{
     if (n == 0)
         return 1;
     if (n == 1)
         return 1;
     return (fibo(n-1)+ fibo(n-2));
 }
-console.log(fibo(2));
+
+console.log(fibo(3)); */
+function fibonacci(n) {
+    let fib = [0, 1]; // Inicializamos la serie con los dos primeros números
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2]; // Calculamos el siguiente número en la serie
+    }
+    return fib.slice(0, n); // Devolvemos los primeros n números de la serie
+}
+
+  console.log(fibonacci(10)); 
+  
+
+// Encontrar el numero n de una figura piramidal triangular (tetrahedron)
+// (nivel) -> cantidad de numeritos
+
+// Crear una funcion que tome el nombre de un pais y su area
+// (nombre, a)
+// Debe regresar la proporcion del area del pais en acorde a el area de tierra en el planeta
+//('Russia', 17098242) -> Russia is 11.48%
+// ("USA", 9372610) -> "USA is 6.29%
+
+// Escribir una funcion que retorne 0 si el input es 1 y 1 si el input es 0
+// No se pueden utilizar condicionales, ternarios, negaciones ni operatores bit
+
+// Messenger bzzz
+// (0 ) n
+// Si no hay nadie -> "No hay nadie en linea"
+// Si hay 1 persona -> "user1 esta en linea"
+// Si hay 2 personas -> "user1 y user2 estan en linea"
+// Si hay n>2 personas, "user1 y n-1 mas estan en linea"
